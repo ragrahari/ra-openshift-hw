f315557cc4ebc71cd19b564c02a9408fe3db54ef
Agrahari <sg0304488@global.ad.sabre.com>
2018-10-30 05:45:25 -0500

    sonarqube updates

--- Infrastructure/bin/setup_sonar.sh
+++ Infrastructure/bin/setup_sonar.sh
@@ -4,7 +4,7 @@ if [ "$#" -ne 1 ]; then
     echo "Usage:"
     echo "  $0 GUID"
 	echo "example: setup_sonar.sh ra"
-	sleep 5
+	sleep 3
     exit 1
 fi
 
@@ -18,12 +18,19 @@ echo "Setting up Sonarqube in project $GUID-sonarqube"
 ### TO TEST ###
 
 oc project ${GUID}-sonarqube
+echo "Deploying persistent postgre database"
 oc new-app --template=postgresql-persistent --param POSTGRESQL_USER=sonar --param POSTGRESQL_PASSWORD=sonar --param POSTGRESQL_DATABASE=sonar --param VOLUME_CAPACITY=4Gi --labels=app=sonarqube_db
-oc new-app --docker-image=wkulhanek/sonarqube:6.7.3 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube
+echo "Deploying SonarQube image"
+oc new-app --docker-image=wkulhanek/sonarqube:6.7.4 --env=SONARQUBE_JDBC_USERNAME=sonar --env=SONARQUBE_JDBC_PASSWORD=sonar --env=SONARQUBE_JDBC_URL=jdbc:postgresql://postgresql/sonar --labels=app=sonarqube
+# Pause rollout and exponse sonarqube service
+oc rollout pause dc sonarqube
+oc expose service sonarqube
 # Create and configure persistent volumen for sonarqube
-oc create -f ../templates/sonarqube_pvc.yaml
+echo "Creating and mounting PVC"
+oc create -f ../templates/pvc-sonar.yaml
 oc set volume dc/sonarqube --add --overwrite --name=sonarqube-volume-1 --mount-path=/opt/sonarqube/data/ --type persistentVolumeClaim --claim-name=sonarqube-pvc
 oc set resources dc/sonarqube --limits=memory=2Gi,cpu=2 --requests=memory=1Gi,cpu=1
+oc patch dc sonarqube --patch='{ "spec": { "strategy": { "type": "Recreate" }}}'
 # Set liveness and readiness probe
 oc set probe dc/sonarqube --liveness --failure-threshold 3 --initial-delay-seconds 40 -- echo ok
 oc set probe dc/sonarqube --readiness --failure-threshold 3 --initial-delay-seconds 20 --get-url= http://:9000/about
