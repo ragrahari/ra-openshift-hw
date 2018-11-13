02f65140cf20a5cc9e8f23ab912744c238cbb824
Rupesh Agrahari <ragrahari@localhost.localdomain>
2018-11-13 10:46:45 -0600

    checkpoint 3

--- Infrastructure/bin/setup_nexus.sh
+++ Infrastructure/bin/setup_nexus.sh
@@ -59,7 +59,7 @@ oc rollout resume deploymentconfig nexus3
 echo "Done!"
 
 ## Once Nexus is deployed, following script can be used to setup the Nexus repository
-curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
-chmod +x setup_nexus3.sh
-./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
-rm setup_nexus3.sh
+#curl -o setup_nexus3.sh -s https://raw.githubusercontent.com/wkulhanek/ocp_advanced_development_resources/master/nexus/setup_nexus3.sh
+#chmod +x setup_nexus3.sh
+#./setup_nexus3.sh admin admin123 http://$(oc get route nexus3 --template='{{ .spec.host }}')
+#rm setup_nexus3.sh
