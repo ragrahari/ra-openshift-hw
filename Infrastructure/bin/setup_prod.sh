ed25907139522bc697312671c1788d8f38fc983c
Rupesh Agrahari <ragrahari@Rupeshs-MacBook-Pro-2.local>
2018-12-05 14:32:53 -0500

    fixing prod setup script

--- Infrastructure/bin/setup_prod.sh
+++ Infrastructure/bin/setup_prod.sh
@@ -20,6 +20,7 @@ oc policy add-role-to-group system:image-puller system:serviceaccounts:${GUID}-p
 # Grant the correct permissions to the Jenkins service account
 oc policy add-role-to-user edit system:serviceaccount:${GUID}-jenkins:jenkins -n ${GUID}-parks-prod
 oc policy add-role-to-user admin system:serviceaccount:gpte-jenkins:jenkins -n ${GUID}-parks-prod
+
 # create headless MongoDB service
 oc create -f ./Infrastructure/templates/mongodb_internal.yaml -n ${GUID}-parks-prod
 # create regular MongoDB service
@@ -64,24 +65,22 @@ oc create configmap mlbparks-blue-config \
  --from-literal=APPNAME="MLB Parks (Blue)" \
  -n ${GUID}-parks-prod
 # Green deployment : APPNAME="MLB Parks (Green)"
-oc new-app ${GUID}-parks-dev/mlbparks --name=mlbparks-green --allow-missing-imagestream-tags=true -l type=parksmap-backend -n ${GUID}-parks-prod
+oc new-app ${GUID}-parks-dev/mlbparks:0.0 --name=mlbparks-green --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+
 oc set triggers dc/mlbparks-green --remove-all -n ${GUID}-parks-prod
-oc set env --from=configmap/mlbparks-green-config dc/mlbparks-green -n ${GUID}-parks-prod
-oc expose dc/mlbparks-green --port 8080 -n ${GUID}-parks-prod
-oc expose svc/mlbparks-green --name=mlbparks -n ${GUID}-parks-prod
-# Probes for green deployment
-oc set probe dc/mlbparks-green --readiness --get-url=http://:8080/ws/healthz/ --failure-threshold=3 --initial-delay-seconds=15 -n ${GUID}-parks-prod
-oc set probe dc/mlbparks-green --liveness --failure-threshold 3 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-prod
+oc set env dc/mlbparks-green --from=configmap/mlbparks-green-config -n ${GUID}-parks-prod
+
 # Blue Deployment : APPNAME="MLB Parks (Blue)"
-oc new-app ${GUID}-parks-dev/mlbparks --name=mlbparks-blue --allow-missing-imagestream-tags=true -l type=parksmap-backend-inactive -n ${GUID}-parks-prod
-oc set resources dc/mlbparks-blue --limits=memory=512Mi,cpu=250m -n ${GUID}-parks-prod
+oc new-app ${GUID}-parks-dev/mlbparks:0.0 --name=mlbparks-blue --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+
+#oc set resources dc/mlbparks-blue --limits=memory=512Mi,cpu=250m -n ${GUID}-parks-prod
 oc set triggers dc/mlbparks-blue --remove-all -n ${GUID}-parks-prod
-oc set env --from=configmap/mlbparks-blue-config dc/mlbparks-blue -n ${GUID}-parks-prod
+oc set env dc/mlbparks-blue --from=configmap/mlbparks-blue-config -n ${GUID}-parks-prod
+
+oc expose dc/mlbparks-green --port 8080 -n ${GUID}-parks-prod
 oc expose dc/mlbparks-blue --port 8080 -n ${GUID}-parks-prod
+oc expose svc/mlbparks-green --name=mlbparks -n ${GUID}-parks-prod --labels="type=parksmap-backend"
 #oc expose svc/mlbparks-blue --name=mlbparks -n ${GUID}-parks-prod
-# Probes for blue deployment
-oc set probe dc/mlbparks-blue --readiness --get-url=http://:8080/ws/healthz/ --failure-threshold=3 --initial-delay-seconds=15 -n ${GUID}-parks-prod
-oc set probe dc/mlbparks-blue --liveness --failure-threshold 3 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-prod
 
 #----- Commands for nationalparks app -----#
 # Green
@@ -105,38 +104,64 @@ oc create configmap nationalparks-blue-config \
  --from-literal=APPNAME="National Parks (Blue)" \
  -n ${GUID}-parks-prod
 # Green Deployment
-oc new-app ${GUID}-parks-dev/nationalparks --name=nationalparks-green --allow-missing-imagestream-tags=true -l type=parksmap-backend -n ${GUID}-parks-prod
+oc new-app ${GUID}-parks-dev/nationalparks:0.0 --name=nationalparks-green --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+
 oc set triggers dc/nationalparks-green --remove-all -n ${GUID}-parks-prod
-oc set env --from=configmap/nationalparks-green-config dc/nationalparks-green -n ${GUID}-parks-prod
-oc expose dc/nationalparks-green --port=8080 -n ${GUID}-parks-prod
-oc expose svc/nationalparks-green --name=nationalparks -n ${GUID}-parks-prod
-# Probes for green deployment
-oc set probe dc/nationalparks-green --readiness --get-url=http://:8080/ws/healthz/ --failure-threshold=3 --initial-delay-seconds=30 -n ${GUID}-parks-prod
-oc set probe dc/nationalparks-green --liveness --failure-threshold 3 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-prod
+oc set env dc/nationalparks-green --from=configmap/nationalparks-green-config -n ${GUID}-parks-prod
+
 # Blue Deployment
-oc new-app ${GUID}-parks-dev/nationalparks --name=nationalparks-blue --allow-missing-imagestream-tags=true -l type=parksmap-backend-inactive -n ${GUID}-parks-prod
+oc new-app ${GUID}-parks-dev/nationalparks:0.0 --name=nationalparks-blue --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+
 oc set triggers dc/nationalparks-blue --remove-all -n ${GUID}-parks-prod
-oc set env --from=configmap/nationalparks-blue-config dc/nationalparks-blue -n ${GUID}-parks-prod
+oc set env dc/nationalparks-blue --from=configmap/nationalparks-blue-config -n ${GUID}-parks-prod
+
+oc expose dc/nationalparks-green --port=8080 -n ${GUID}-parks-prod
 oc expose dc/nationalparks-blue --port=8080 -n ${GUID}-parks-prod
+oc expose svc/nationalparks-green --name=nationalparks -n ${GUID}-parks-prod --labels="type=parksmap-backend"
 #oc expose svc/nationalparks-blue --name=nationalparks -n ${GUID}-parks-prod
-# Probes for blue deployment
-oc set probe dc/nationalparks-blue --readiness --get-url=http://:8080/ws/healthz/ --failure-threshold=3 --initial-delay-seconds=30 -n ${GUID}-parks-prod
-oc set probe dc/nationalparks-blue --liveness --failure-threshold 3 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-prod
 
 #----- Commands for parksmap app -----#
 # Green
-oc new-app ${GUID}-parks-dev/parksmap --name=parksmap-green --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+oc new-app ${GUID}-parks-dev/parksmap:0.0 --name=parksmap-green --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+
 oc set triggers dc/parksmap-green --remove-all -n ${GUID}-parks-prod
+
+oc set env dc/parksmap-green --from=configmap/parksmap-green-config -n ${GUID}-parks-prod
+
+# Blue
+oc new-app ${GUID}-parks-dev/parksmap:0.0 --name=parksmap-blue --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
+
+oc set triggers dc/parksmap-blue --remove-all -n ${GUID}-parks-prod
+
+oc set env dc/parksmap-blue --from=configmap/parksmap-blue-config -n ${GUID}-parks-prod
+
 oc expose dc/parksmap-green --port=8080 -n ${GUID}-parks-prod
+oc expose dc/parksmap-blue --port=8080 -n ${GUID}-parks-prod
 oc expose svc/parksmap-green --name=parksmap -n ${GUID}-parks-prod
-# Probes
+#oc expose svc/parksmap-blue --name=parksmap -n ${GUID}-parks-prod
+
+# Deployment hooks
+oc set deployment-hook dc/mlbparks-green  -n ${GUID}-parks-prod --post -c mlbparks-green --failure-policy=ignore -- curl http://mlbparks-green.${GUID}-parks-prod.svc.cluster.local:8080/ws/data/load/
+oc set deployment-hook dc/nationalparks-green  -n ${GUID}-parks-prod --post -c nationalparks-green --failure-policy=ignore -- curl http://nationalparks-green.${GUID}-parks-prod.svc.cluster.local:8080/ws/data/load/
+
+oc set deployment-hook dc/mlbparks-blue  -n ${GUID}-parks-prod --post -c mlbparks-blue --failure-policy=ignore -- curl http://mlbparks-blue.${GUID}-parks-prod.svc:8080/ws/data/load/
+oc set deployment-hook dc/nationalparks-blue  -n ${GUID}-parks-prod --post -c nationalparks-blue --failure-policy=ignore -- curl http://nationalparks-blue.${GUID}-parks-prod.svc:8080/ws/data/load/
+
+# Probes for mlbparks green deployment
+oc set probe dc/mlbparks-green --readiness --get-url=http://:8080/ws/healthz/ --failure-threshold=3 --initial-delay-seconds=15 -n ${GUID}-parks-prod
+oc set probe dc/mlbparks-green --liveness --failure-threshold 3 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-prod
+# Probes for mlbparks blue deployment
+oc set probe dc/mlbparks-blue --readiness --get-url=http://:8080/ws/healthz/ --failure-threshold=3 --initial-delay-seconds=15 -n ${GUID}-parks-prod
+oc set probe dc/mlbparks-blue --liveness --failure-threshold 3 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-prod
+# Probes for nationalparks green deployment
+oc set probe dc/nationalparks-green --readiness --get-url=http://:8080/ws/healthz/ --failure-threshold=3 --initial-delay-seconds=30 -n ${GUID}-parks-prod
+oc set probe dc/nationalparks-green --liveness --failure-threshold 3 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-prod
+# Probes for nationalparks blue deployment
+oc set probe dc/nationalparks-blue --readiness --get-url=http://:8080/ws/healthz/ --failure-threshold=3 --initial-delay-seconds=30 -n ${GUID}-parks-prod
+oc set probe dc/nationalparks-blue --liveness --failure-threshold 3 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-prod
+# Probes for parksmap green deployment
 oc set probe dc/parksmap-green --readiness --get-url=http://:8080/ws/healthz/ --failure-threshold=3 --initial-delay-seconds=15 -n ${GUID}-parks-prod
 oc set probe dc/parksmap-green --liveness --failure-threshold 3 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-prod
-# Blue
-oc new-app ${GUID}-parks-dev/parksmap --name=parksmap-blue --allow-missing-imagestream-tags=true -n ${GUID}-parks-prod
-oc set triggers dc/parksmap-blue --remove-all -n ${GUID}-parks-prod
-oc expose dc/parksmap-blue --port=8080 -n ${GUID}-parks-prod
-#oc expose svc/parksmap-blue --name=parksmap -n ${GUID}-parks-prod
-# Probes
+# Probes for parksmap blue deployment
 oc set probe dc/parksmap-blue --readiness --get-url=http://:8080/ws/healthz/ --failure-threshold=3 --initial-delay-seconds=15 -n ${GUID}-parks-prod
 oc set probe dc/parksmap-blue --liveness --failure-threshold 3 --initial-delay-seconds 30 -- echo ok -n ${GUID}-parks-prod
